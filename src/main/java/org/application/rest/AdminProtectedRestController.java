60617e1531ff6864056591937ef36c8c7e799979
Vadim Mashenko <vadim.mashenko@solidlab.ru>
2025-02-06 16:33:38 +0700

    Added error msg

--- src/main/java/org/application/rest/AdminProtectedRestController.java
+++ src/main/java/org/application/rest/AdminProtectedRestController.java
@@ -3,7 +3,14 @@ package org.application.rest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +21,12 @@ public class AdminProtectedRestController {
       return ResponseEntity.ok(new HiddenMessage("this is a hidden message!"));
    }
 
+   @GetMapping("/errormessage")
+   public void redirectErrorMsg(@RequestParam("error") String error,
+                                HttpServletResponse response) throws IOException {
+      String encodedError = URLEncoder.encode(error, StandardCharsets.UTF_8.toString());
+      response.sendRedirect("/index.html?error=" + encodedError);
+   }
    private static class HiddenMessage {
 
       private final String message;
