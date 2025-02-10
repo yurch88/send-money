296a7bdb9c21508b28bba22729496344818634f4
Vadim Mashenko <vadim.mashenko@solidlab.ru>
2025-02-10 10:55:19 +0700

    Return endpoint

--- src/main/java/org/application/rest/AdminProtectedRestController.java
+++ src/main/java/org/application/rest/AdminProtectedRestController.java
@@ -21,6 +21,12 @@ public class AdminProtectedRestController {
       return ResponseEntity.ok(new HiddenMessage("this is a hidden message!"));
    }
 
+   @GetMapping("/errormessage")
+   public void redirectErrorMsg(@RequestParam("error") String error,
+                                HttpServletResponse response) throws IOException {
+      String encodedError = URLEncoder.encode(error, StandardCharsets.UTF_8.toString());
+      response.sendRedirect("/index.html?error=" + encodedError);
+   }
    private static class HiddenMessage {
 
       private final String message;
